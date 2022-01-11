9186d14f4cb04c9cf515cf99e3d876e50c2852bf
Taras <tarasparakhin@gmail.com>
2022-01-12 00:06:07 +0200

    classwork_1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-
-import {getMissions} from "./services/serviceMissions";
 import Missions from "./components/Missions";
-import {useEffect, useState} from "react"
 
 function App() {
-  let [missions, setMissions] = useState([]);
-
-  useEffect(() => {
-    getMissions().then(value => setMissions(value.filter(item => item.launch_year !== '2020')))
-  },[]);
 
   return (
     <div className="App">
-      {
-        missions.map((mission) => <Missions key={mission.flight_number} mission={mission}/>)
-      }
+      <Missions/>
     </div>
   );
 }
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,16 +1,26 @@
 import './Mission.css';
+import {useEffect, useState} from "react";
+import {getMissions} from "../services/serviceMissions";
 
 function Missions(props)  {
-    let {mission: {mission_name, launch_year, links: {mission_patch_small}}} = props
+    let [missions, setMissions] = useState([]);
+
+    useEffect(() => {
+        getMissions().then(value => setMissions(value.filter(item => item.launch_year !== '2020')))
+    },[]);
 
     return (
-        <div className='mission'>
-            <div>
-                <h1>{mission_name}</h1>
-                <div>{launch_year}</div>
-            </div>
-            <img src={mission_patch_small} alt='photo'/>
-        </div>
+        <>
+            {missions.map(mission =>
+                <div key={mission.flight_number} className='mission'>
+                    <div>
+                        <h1>{mission.mission_name}</h1>
+                        <div>{mission.launch_year}</div>
+                    </div>
+                    <img src={mission.links.mission_patch_small} alt='photo'/>
+                </div>)
+            }
+        </>
     )
 }
 
