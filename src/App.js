0adc5cf4b39734149f3b2155501e79a812e13e03
Taras <tarasparakhin@gmail.com>
2022-01-11 23:57:43 +0200

    homework_1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,18 @@
 import './App.css';
 
-import {useEffect, useState} from "react";
-
-import {getUsers} from "./components/services/serviceUsers";
-import {getPosts} from "./components/services/servicePosts";
-import {getComments} from "./components/services/serviceComments";
-
 import Users from "./components/users/Users";
 import Posts from "./components/posts/Posts";
 import Comments from "./components/comments/Comments";
 
 function App() {
-  let [users, setUsers] = useState([]),
-      [posts, setPosts] = useState([]),
-      [comments, setComments] = useState([]);
-
-  useEffect(() => {
-    getUsers().then(value => setUsers(value))
-  },[]);
-
-  useEffect(() => {
-    getPosts().then(value => setPosts(value))
-  },[]);
-
-  useEffect(() => {
-    getComments().then(value => setComments(value))
-  },[]);
-
   return (
     <div className="App">
       <div className="wrap">
-        <div>{
-            users.map(user => <Users
-                key={user.id}
-                user={user}
-            />)
-          }</div>
-
-        <div className='posts'>{
-            posts.map(post => <Posts
-                key={post.id}
-                post={post}
-            />)
-          }</div>
+        <div>{<Users/>}</div>
+        <div className='posts'>{<Posts/>}</div>
       </div>
 
-      {
-        comments.map(comment => <Comments
-            key={comment.id}
-            comment={comment}
-        />)
-      }
+        <div className='comments'>{<Comments/>}</div>
     </div>
   );
 }
