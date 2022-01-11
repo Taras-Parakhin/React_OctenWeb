3eb2e6da9e92bffaf0bf760b8703a3db9e33e029
Taras <tarasparakhin@gmail.com>
2022-01-11 21:46:57 +0200

    homework_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
+import {useEffect, useState} from "react";
+
+import {getUsers} from "./components/services/serviceUsers";
+import {getPosts} from "./components/services/servicePosts";
+import {getComments} from "./components/services/serviceComments";
+
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+import Comments from "./components/comments/Comments";
+
 function App() {
+  let [users, setUsers] = useState([]),
+      [posts, setPosts] = useState([]),
+      [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(value => setUsers(value))
+  },[]);
+
+  useEffect(() => {
+    getPosts().then(value => setPosts(value))
+  },[]);
+
+  useEffect(() => {
+    getComments().then(value => setComments(value))
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrap">
+        <div>{
+            users.map(user => <Users
+                key={user.id}
+                user={user}
+            />)
+          }</div>
+
+        <div className='posts'>{
+            posts.map(post => <Posts
+                key={post.id}
+                post={post}
+            />)
+          }</div>
+      </div>
+
+      {
+        comments.map(comment => <Comments
+            key={comment.id}
+            comment={comment}
+        />)
+      }
     </div>
   );
 }
---
+++ src/components/services/serviceComments.js
@@ -0,0 +1,11 @@
+const getComments = () => {
+    return fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(value => value.json())
+}
+
+const getComment = (id) => {
+    return fetch('https://jsonplaceholder.typicode.com/comments' + id)
+        .then(value => value.json())
+}
+
+export {getComments, getComment};
