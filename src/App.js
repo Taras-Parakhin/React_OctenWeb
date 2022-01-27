94d87c5d7d0225ae8d7f06294d015be9df869b50
Taras Parakhin <86777880+Taras-Parakhin@users.noreply.github.com>
2022-01-27 18:10:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,3 +42,33 @@ function App() {
 }
 
 export default App;
+
+
+// import {useState} from "react";
+//
+// import './Form.css';
+//
+// const Form = (getFilter) => {
+//
+//     const [form, setForm] = useState({name: '', username: '', email: ''});
+//
+//     const formHandler = (e) => {
+//         const data = {...form, [e.target.name]: e.target.value}
+//         setForm({...data})
+//         getFilter(data)
+//     }
+//
+//     return (
+//         <form className='form'>
+//             <div><label>Name: <input type="text" name={'name'} value={form.name} onChange={formHandler}/></label></div>
+//             <div><label>Username: <input type="text" name={'username'} value={form.username} onChange={formHandler}/></label></div>
+//             <div><label>Email: <input type="text" name={'email'} value={form.email} onChange={formHandler}/></label></div>
+//             <button>Send</button>
+//         </form>
+//     )
+//
+// }
+//
+// export default Form;
+
+
